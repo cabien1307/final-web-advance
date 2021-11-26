3be0c4bc50c2c7642565b68528dfe669a23e4ce9
cabien1307 <cabien13072000@gmail.com>
2021-11-26 21:58:56 +0700

    Load 10 post infinte

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,38 +4,92 @@ import { useSelector } from "react-redux";
 import LoadIcon from "../../images/loading.gif";
 import Post from "../../components/Post/Post";
 import NoPost from "../../images/post.svg";
+import './home.css'
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState } from "react";
 
 function Home() {
     const { homePosts } = useSelector((state) => state);
     const { notifications } = useSelector((state) => state.notify)
 
+    const [posts, setPosts] = useState(
+        {
+            hasMore: true,
+            items:
+                (homePosts.result === 0 || homePosts.posts.length === 0)
+                    ? homePosts.posts.slice(0, 1)
+                    : []
+        }
+    )
+
+    const fetchMoreData = () => {
+
+        if (posts.items.length >= homePosts.posts.length) {
+            setPosts({
+                hasMore: false,
+                items: [...posts.items]
+            })
+            return;
+        }
+
+        setTimeout(() => {
+            setPosts({
+                hasMore: true,
+                items: [
+                    ...posts.items,
+                    ...homePosts.posts.slice(posts.items.length, posts.items.length + 1)
+                ]
+            })
+        }, 500);
+    };
 
     return (
         <>
             <div className="col-span-6 2xl:col-span-6 xl:col-span-6 lg:col-span-10 md:col-span-10 sm:col-span-10 sm:gap-0 border-r-2">
                 <Status />
-                {homePosts.loading ? (
-                    <img
-                        src={LoadIcon}
-                        alt="loading"
-                        className="block mx-auto"
-                    />
-                ) : (homePosts.result === 0 || homePosts.posts.length === 0) ? (
-                    <div
-                        className="list-posts mx-7 my-5 xl:mx-auto lg:mx-2 md:mx-2 sm:mx-1 xs:mx-1"
-                    >
+                <div className="pppp">
+
+                    {homePosts.loading ? (
                         <img
-                            src={NoPost}
-                            alt="NoPost"
-                            className="w-1/2 h-auto object-cover mx-auto"
+                            src={LoadIcon}
+                            alt="loading"
+                            className="block mx-auto"
                         />
-                        <h1 className="text-center text-xl mt-2 text-red-500 font-semibold">
-                            Publish new post to see something !
-                        </h1>
-                    </div>
-                ) : (
-                    homePosts.posts.map((post, index) => <Post key={index} post={post} />)
-                )}
+                    ) : (homePosts.result === 0 || homePosts.posts.length === 0) ? (
+                        <div
+                            className="list-posts mx-7 my-5 xl:mx-auto lg:mx-2 md:mx-2 sm:mx-1 xs:mx-1"
+                        >
+                            <img
+                                src={NoPost}
+                                alt="NoPost"
+                                className="w-1/2 h-auto object-cover mx-auto"
+                            />
+                            <h1 className="text-center text-xl mt-2 text-red-500 font-semibold">
+                                Publish new post to see something !
+                            </h1>
+                        </div>
+                    ) : (
+                        <InfiniteScroll
+                            dataLength={posts.items.length}
+                            next={fetchMoreData}
+                            hasMore={posts.hasMore}
+                            loader={<h4>Loading...</h4>}
+                            endMessage={
+                                <p style={{ textAlign: "center" }}>
+                                    <b>Yay! You have seen it all</b>
+                                </p>
+                            }
+                        >
+                            {
+                                posts.items.map((post, index) =>
+                                    <Post key={index} post={post} />
+                                )
+                            }
+                        </InfiniteScroll>
+
+                    )}
+                </div>
             </div>
             <RightBar notifications={notifications} />
         </>
